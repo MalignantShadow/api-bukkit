c378cd132ccd4ed8e1cdb90694f2a22f23f313f6
MalignantShadow <malignant.shadowborne@gmail.com>
2016-12-27 15:30:17 -0800

    Add config helper

---
+++ src/info/malignantshadow/api/bukkit/helpers/BukkitConfigs.java
@@ -0,0 +1,14 @@
+package info.malignantshadow.api.bukkit.helpers;
+
+public class BukkitConfigs {
+	
+	/**
+	 * TODO: get/put for
+	 * 
+	 * colors
+	 * location
+	 * material/blocks+data e.g. ItemStack
+	 * 
+	 */
+	
+}
