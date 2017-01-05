fae9f1d592d9758665f79e8dd68e03442993071c
MalignantShadow <malignant.shadowborne@gmail.com>
2017-01-05 04:41:14 -0800

    Add locations to BukkitConfig

--- src/info/malignantshadow/api/bukkit/helpers/BukkitConfigs.java
+++ src/info/malignantshadow/api/bukkit/helpers/BukkitConfigs.java
@@ -1,15 +1,20 @@
 package info.malignantshadow.api.bukkit.helpers;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Color;
+import org.bukkit.Location;
+import org.bukkit.World;
 
 import info.malignantshadow.api.bukkit.commands.BukkitArgumentTypes;
 import info.malignantshadow.api.config.ConfigPairing;
 import info.malignantshadow.api.config.ConfigSection;
 import info.malignantshadow.api.config.ConfigSequence;
+import info.malignantshadow.api.config.Configs;
 
 public class BukkitConfigs {
 	
 	public static final Color DEF_COLOR = Color.BLACK;
+	public static final Location DEF_LOCATION = new Location(Bukkit.getWorlds().get(0), 0, 0, 0);
 	
 	private BukkitConfigs() {
 	}
@@ -17,8 +22,6 @@ public class BukkitConfigs {
 	/**
 	 * TODO: get/put for
 	 * 
-	 * colors
-	 * location
 	 * material/blocks+data e.g. ItemStack
 	 * 
 	 */
@@ -28,22 +31,7 @@ public class BukkitConfigs {
 	}
 	
 	public static Color getColor(Color def, ConfigPairing pair) {
-		if (pair == null)
-			return def;
-		
-		if (pair.isSection())
-			return getColor(def, pair.asSection());
-		if (pair.isSequence())
-			return getColor(def, pair.asSequence());
-		if (pair.isNumber())
-			return Color.fromRGB(pair.asInt());
-		if (pair.isString()) {
-			Color color = BukkitArgumentTypes.COLOR.getValue(pair.asString());
-			if (color != null)
-				return color;
-		}
-		
-		return def;
+		return Configs.getComplex(def, pair, BukkitConfigs::getColor, BukkitConfigs::getColor, BukkitArgumentTypes.COLOR);
 	}
 	
 	public static Color getColor(ConfigSequence seq) {
@@ -144,6 +132,83 @@ public class BukkitConfigs {
 		seq.add(color.getRed());
 		seq.add(color.getGreen());
 		seq.add(color.getBlue());
+		return seq;
+	}
+	
+	public static Location getLocation(ConfigPairing pair) {
+		return getLocation(DEF_LOCATION, pair);
+	}
+	
+	public static Location getLocation(Location def, ConfigPairing pair) {
+		return Configs.getComplex(def, pair, BukkitConfigs::getLocation, BukkitConfigs::getLocation, BukkitArgumentTypes.LOCATION);
+	}
+	
+	public static Location getLocation(ConfigSection section) {
+		return getLocation(DEF_LOCATION, section);
+	}
+	
+	public static Location getLocation(Location def, ConfigSection section) {
+		if (section == null)
+			return null;
+		
+		World w = Bukkit.getWorld(section.getStringWithDefault("", "world"));
+		double x = section.getNumber(0, "x").doubleValue();
+		double y = section.getNumber(0, "y").doubleValue();
+		double z = section.getNumber(0, "z").doubleValue();
+		float yaw = section.getNumber(0, "yaw").floatValue();
+		float pitch = section.getNumber(0, "pitch").floatValue();
+		return new Location(w, x, y, z, yaw, pitch);
+	}
+	
+	public static Location getLocation(ConfigSequence sequence) {
+		return getLocation(DEF_LOCATION, sequence);
+	}
+	
+	public static Location getLocation(Location def, ConfigSequence sequence) {
+		if (sequence == null)
+			return null;
+		
+		String input = "";
+		for (int i = 0; i < sequence.size(); i++)
+			input += sequence.getString("", i);
+		
+		return BukkitArgumentTypes.LOCATION.getValue(input);
+	}
+	
+	public static String putLocationAsString(Location loc) {
+		if (loc == null)
+			return null;
+		
+		String str = "";
+		World w = loc.getWorld();
+		if (w != null)
+			str += w.getName();
+		return str + loc.getX() + "," + loc.getY() + "," + loc.getZ() + "," + loc.getYaw() + "," + loc.getPitch();
+	}
+	
+	public static ConfigSection putLocationAsSection(Location loc) {
+		ConfigSection section = new ConfigSection();
+		World w = loc.getWorld();
+		if (w != null)
+			section.set(w.getName(), "world");
+		section.set(loc.getX(), "x");
+		section.set(loc.getY(), "y");
+		section.set(loc.getZ(), "z");
+		section.set(loc.getYaw(), "yaw");
+		section.set(loc.getPitch(), "pitch");
+		return section;
+	}
+	
+	public static ConfigSequence putLocationAsSequence(Location loc) {
+		ConfigSequence seq = new ConfigSequence();
+		World w = loc.getWorld();
+		if (w != null)
+			seq.add(w.getName());
+		seq.add(loc.getX());
+		seq.add(loc.getY());
+		seq.add(loc.getZ());
+		seq.add(loc.getYaw());
+		seq.add(loc.getPitch());
 		return seq;
 	}
 	
