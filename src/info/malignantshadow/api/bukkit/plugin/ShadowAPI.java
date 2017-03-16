5023e8a4b75b6d1e6b2360228b3a2b402644d1d0
Caleb Downs <malignant.shadowborne@gmail.com>
2017-03-16 12:55:27 -0700

    Convenience method

--- src/info/malignantshadow/api/bukkit/plugin/ShadowAPI.java
+++ src/info/malignantshadow/api/bukkit/plugin/ShadowAPI.java
@@ -1,24 +1,31 @@
 package info.malignantshadow.api.bukkit.plugin;
 
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+
 import info.malignantshadow.api.bukkit.toys.inventory.VirtualInventoryListener;
 
 public class ShadowAPI extends ShadowPlugin {
-	
+
 	private static ShadowAPI _instance;
-	
+
 	public static final ShadowAPI getInstance() {
 		return _instance;
 	}
-	
+
 	@Override
 	public void onStart() {
 		_instance = this;
 		registerEvents(new VirtualInventoryListener());
 	}
-	
+
 	@Override
 	public void onEnd() {
-		
+
 	}
-	
+
+	public static void registerEvents(Plugin plugin, Listener listener) {
+		plugin.getServer().getPluginManager().registerEvents(listener, plugin);
+	}
+
 }
--- src/info/malignantshadow/api/bukkit/plugin/ShadowPlugin.java
+++ src/info/malignantshadow/api/bukkit/plugin/ShadowPlugin.java
@@ -5,70 +5,70 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public abstract class ShadowPlugin extends JavaPlugin {
-	
+
 	public abstract void onStart();
-	
+
 	public abstract void onEnd();
-	
+
 	@Override
 	public void onEnable() {
 		onStart();
 	}
-	
+
 	@Override
 	public void onDisable() {
 		onEnd();
 	}
-	
+
 	public void registerEvents(Listener listener) {
-		getServer().getPluginManager().registerEvents(listener, this);
+		ShadowAPI.registerEvents(this, listener);
 	}
-	
+
 	public BukkitRunnable createRunnable(Runnable r) {
 		return new BukkitRunnable() {
-			
+
 			@Override
 			public void run() {
 				r.run();
 			}
-			
+
 		};
 	}
-	
+
 	public BukkitRunnable runTask(Runnable r) {
 		BukkitRunnable br = createRunnable(r);
 		br.runTask(this);
 		return br;
 	}
-	
+
 	public BukkitRunnable runTaskLater(long delay, Runnable r) {
 		BukkitRunnable br = createRunnable(r);
 		br.runTaskLater(this, delay);
 		return br;
 	}
-	
+
 	public BukkitRunnable runTaskTimer(long delay, long period, Runnable r) {
 		BukkitRunnable br = createRunnable(r);
 		br.runTaskTimer(this, delay, period);
 		return br;
 	}
-	
+
 	public BukkitRunnable runTaskAsync(Runnable r) {
 		BukkitRunnable br = createRunnable(r);
 		br.runTaskAsynchronously(this);
 		return br;
 	}
-	
+
 	public BukkitRunnable runTaskLaterAsync(long delay, Runnable r) {
 		BukkitRunnable br = createRunnable(r);
 		br.runTaskLaterAsynchronously(this, delay);
 		return br;
 	}
-	
+
 	public BukkitRunnable runTaskTimerAsync(long delay, long period, Runnable r) {
 		BukkitRunnable br = createRunnable(r);
 		br.runTaskTimerAsynchronously(this, delay, period);
 		return br;
 	}
-	
+
 }
